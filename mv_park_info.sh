49b2f817e4611d15ed9cfbe6066ff58732e4583b
bill <zeng.cae37@gmail.com>
2024-10-18 10:39:42 +0800

    refactor: move crawler directory

--- mv_park_info.sh
+++ mv_park_info.sh
@@ -4,7 +4,7 @@
 SCRIPT_DIR=$(dirname "$0")
 
 # Define source and destination directories
-SOURCE_DIR="$SCRIPT_DIR/crawler/output"
+SOURCE_DIR="$SCRIPT_DIR/crawler/parkinfo/output"
 DEST_DIR="$SCRIPT_DIR/webserver/storage/app/private/parkinfo"
 
 # Create destination directory if it doesn't exist
